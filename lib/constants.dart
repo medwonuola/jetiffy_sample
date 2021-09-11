c5adfc9aa4e7ce0bac8635a478a454a6aa52ff68
Arigbabu Abdul-Ahmed <Medwonuola@gmail.com>
2021-09-11 08:09:49 +0100

    added black variation

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xFF0AC690);
 const Color kRedVariation = Color(0xFFE82727);
+const Color kBlackVariation = Color(0xFF1A1A1A);
