83b9e66ace090362f914447a8cb8d0ca63d1cc1e
Arigbabu Abdul-Ahmed <Medwonuola@gmail.com>
2021-09-12 08:49:02 +0100

    Incoming order view

--- lib/screens/incoming_order.dart
+++ lib/screens/incoming_order.dart
@@ -1,1 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:jetiffy/components/address_text.dart';
+import 'package:jetiffy/components/back_widget.dart';
+import 'package:jetiffy/components/time_distance.dart';
+import '../constants.dart';
+
+class IncomingOrder extends StatefulWidget {
+  const IncomingOrder({Key? key}) : super(key: key);
+
+  @override
+  _IncomingOrderState createState() => _IncomingOrderState();
+}
+
+class _IncomingOrderState extends State<IncomingOrder> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double heightFactor = size.height / 954;
+    double widthFactor = size.width / 414;
+
+    final buttonStyle = ElevatedButton.styleFrom(
+        elevation: 6,
+        textStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+        minimumSize: Size(173 * widthFactor, 47 * heightFactor));
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          "Incoming Order",
+          style: TextStyle(
+              fontWeight: FontWeight.w700, fontSize: 22.5, color: Colors.black),
+        ),
+        leading: const BackWidget(),
+      ),
+      body: Stack(
+        children: [
+          Container(),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Card(
+                elevation: 15,
+                shadowColor: Colors.black.withOpacity(0.1),
+                child: Container(
+                  width: double.infinity,
+                  height: 552 * heightFactor,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(24),
+                          topLeft: Radius.circular(24))),
+                  child: Stack(
+                    children: [
+                      const Align(
+                          alignment: Alignment.topCenter,
+                          child: SizedBox(
+                            width: 40,
+                            child:
+                                Divider(color: Color(0xFFF2F2F2), thickness: 3),
+                          )),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: 44 * heightFactor, right: 32 * widthFactor),
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: const BoxDecoration(
+                                color: kPrimaryColor, shape: BoxShape.circle),
+                            child: const Center(
+                                child: Icon(
+                              Icons.call,
+                              color: Colors.white,
+                            )),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24 * widthFactor,
+                            vertical: 40 * heightFactor),
+                        child: Column(
+                          children: [
+                            const AddressText(
+                              title: "Sender's",
+                              address:
+                                  "7 Prince Ibrahim Odofin Street Idado Estate Igbo-Efon",
+                            ),
+                            const SizedBox(height: 8),
+                            const AddressText(
+                              title: "Receiver",
+                              address:
+                                  "7 Prince Ibrahim Estate Igbo-Efon 7 Prince Ibrahim Odofin Street Idado",
+                            ),
+                            const SizedBox(height: 3),
+                            const TimeAndDistance(
+                                time: "20 secs", distance: "2.8 km"),
+                            const Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    style: buttonStyle.copyWith(
+                                      shadowColor: MaterialStateProperty.all(
+                                          kRedVariation.withOpacity(0.34)),
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              kRedVariation),
+                                    ),
+                                    child: const Text("Reject")),
+                                ElevatedButton(
+                                    onPressed: () {},
+                                    style: buttonStyle.copyWith(
+                                      shadowColor: MaterialStateProperty.all(
+                                          kBlackVariation.withOpacity(0.34)),
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              kBlackVariation),
+                                    ),
+                                    child: const Text("Accept Trip")),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
